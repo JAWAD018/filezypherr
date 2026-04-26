96105ed2cd3efaceedb6382d9b2eb6ff4fff51f1
Mohammed Jawad <115097046+JAWAD018@users.noreply.github.com>
2026-04-26 15:43:47 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://filezypherr.vercel.app";
+  const baseUrl = "https://filezypher.vercel.app";
 
   const routes = [
     "",
